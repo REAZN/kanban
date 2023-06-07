cb3c74c86e972b5918b58885c48bad70e1edb14c
REAZN <lewis@reazn.me>
2023-06-07 01:25:06 +0100

    nav divider

---
+++ src/assets/icons/Divider.tsx
@@ -0,0 +1,10 @@
+
+
+
+export const Divider = () => (
+  <svg className="with-icon_icon__aLCKg" data-testid="geist-icon" fill="none" height="32"
+       shapeRendering="geometricPrecision" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round"
+       strokeWidth="1" viewBox="0 0 24 24" width="32" style={{color: "var(--accents-2)"}}>
+    <path d="M16.88 3.549L7.12 20.451"></path>
+  </svg>
+)
