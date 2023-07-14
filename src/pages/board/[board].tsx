df0689fe986424c543b62108cf185fb970e8dd63
REAZN <lewis@reazn.me>
2023-07-14 02:25:58 +0100

    working list positioning

--- src/pages/board/[board].tsx
+++ src/pages/board/[board].tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { type NextPage } from "next";
 import Head from "next/head";
 import { Header, List } from "@/components";
@@ -5,7 +7,6 @@ import { useEffect, useState } from "react";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
 import { api, type RouterOutputs } from "@/utils/api";
-import { Button } from "@/components/ui/button";
 import {
   DragDropContext,
   Droppable,
@@ -14,7 +15,7 @@ import {
 import { Plus } from "lucide-react";
 
 import { Testy } from "@/components/board/Testy";
-
+import { useBoardStore } from "@/store";
 
 type List = RouterOutputs["list"]["getAll"][0];
 
@@ -22,9 +23,15 @@ const Board: NextPage = () => {
   const { data: sessionData } = useSession();
   const router = useRouter();
 
-  const [listIDD, setListIDD] = useState<string>("cli8a6bga0001v7wc5a5omxu8")
-  const [listPOSS, setListPOSS] = useState<number>(1)
+  //Temp SSR fix...
+  const [isBrowser, setIsBrowser] = useState<boolean>(false);
 
+  //Temp SSR fix...
+  useEffect(() => {
+    setIsBrowser(typeof window !== "undefined" ? true : false);
+  }, []);
+
+  const { updateListPos, setLists, addList, lists } = useBoardStore();
 
   const { data: listsData, refetch: refetchLists } = api.list.getAll.useQuery(
     {
@@ -36,37 +43,44 @@ const Board: NextPage = () => {
     }
   );
 
-  const [lists, setLists] = useState<List[] | undefined>(listsData);
-
-  const penis = api.list.updatePos.useMutation({
+  const list = api.list.updatePos.useMutation({
     onSuccess: () => {
-      console.log("done!")
-    }
-  })
+      console.log("done!");
+      void refetchLists();
+    },
+    onMutate: (data) => {
+      console.log(data);
+    },
+  });
 
-  // const [lists, setLists] = useState<List[] | undefined>();
+  const task = api.task.updatePos.useMutation({
+    onSuccess: () => {
+      console.log("done!");
+      void refetchLists();
+    },
+    onMutate: (data) => {
+      console.log(data);
+    },
+  });
 
   useEffect(() => {
-    setLists(listsData);
-  }, [listsData]);
-
-  // const { data: tasks, refetch: refetchTasks } = api.task.getAll.useQuery({
-  //   boardId: String(router.query.board),
-  // });
-
+    setLists(listsData || []);
+  }, [setLists, listsData]);
 
   const createList = api.list.create.useMutation({
     onSuccess: () => {
       void refetchLists();
     },
+    onMutate: (data) => {
+      addList({
+        boardId: data.boardId,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        id: String(Math.random()),
+        name: data.name,
+      } as List);
+    },
   });
-
-
-  // const updateTask = api.task.update.useMutation({
-  //   onSuccess: () => {
-  //     void refetchTasks();
-  //   },
-  // });
 
   const onDragEnd = (result: DropResult) => {
     if (!result.destination) return;
@@ -75,52 +89,70 @@ const Board: NextPage = () => {
     console.log(
       `destination: ${result.destination.index},
        source: ${result.source.index},
-       draggableId: ${result.draggableId}`
+       draggableId: ${result.draggableId} ${JSON.stringify(result.source)}`
     );
 
     if (result.type === "list") {
-
+      list.mutate({
+        boardId: String(router.query.board),
+        listId: result.draggableId,
+        newPosition: result.destination.index,
+      });
+      updateListPos(
+        result.draggableId,
+        result.destination.index,
+        result.source.index
+      );
     }
 
     if (result.type === "task") {
-
+      task.mutate({
+        boardId: String(router.query.board),
+        listId: "clj30rn6h0019v7fgcsyr9047",
+        taskId: result.draggableId,
+        newPosition: result.destination.index,
+      });
     }
-
-
-    // updateTask.mutate({
-    //   id: result.draggableId,
-    // });
   };
 
   return (
     <>
       <Head>
+        {/* TODO global title prefix */}
         <title>Kanban - Board</title>
-        <meta name="description" content="Kanban" />
-        <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
-      <section className="flex flex-row m-8">
-        <Testy/>
-        <Button onClick={() => penis.mutate({
-          boardId: String(router.query.board),
-          listId: listIDD,
-          newPosition: listPOSS
-        })}
-        >change order</Button>
-        <DragDropContext onDragEnd={onDragEnd}>
-          <Droppable droppableId={String(router.query.board)} direction="horizontal" type="list">
-            {(provided) => (
-              <div className="flex my-5 gap-4" {...provided.droppableProps} ref={provided.innerRef}>
-                {lists?.map((list, index) => (
-                  <List key={list.id} list={list} index={index} refresh={refetchLists} />
-                ))}
-              </div>
-            )}
-          </Droppable>
-        </DragDropContext>
+      <section className="m-8 mt-20 flex flex-row">
+        {isBrowser && (
+          <DragDropContext onDragEnd={onDragEnd}>
+            <Droppable
+              droppableId={String(router.query.board)}
+              direction="horizontal"
+              type="list"
+            >
+              {(provided) => (
+                <div
+                  className="my-5 flex"
+                  {...provided.droppableProps}
+                  ref={provided.innerRef}
+                >
+                  {lists?.map((list, index) => (
+                    <List
+                      key={list.id}
+                      list={list}
+                      index={index}
+                      refresh={refetchLists}
+                      className="mx-2"
+                    />
+                  ))}
+                  {provided.placeholder}
+                </div>
+              )}
+            </Droppable>
+          </DragDropContext>
+        )}
         <button
-          className="w-64 h-fit text-sm flex gap-2 mt-1 hover:bg-accent flex items-center justify-left pl-1 py-1.5 rounded-md"
+          className="justify-left mt-1 flex h-fit w-64 items-center gap-2 rounded-md py-1.5 pl-1 text-sm hover:bg-accent"
           onClick={() =>
             createList.mutate({
               boardId: String(router.query.board),
@@ -128,7 +160,7 @@ const Board: NextPage = () => {
             })
           }
         >
-          <Plus className="h-4 w-4 shrink-0 opacity-50"/>
+          <Plus className="h-4 w-4 shrink-0 opacity-50" />
           <span className="opacity-50">New</span>
         </button>
       </section>
