d68be35049014c1f0635e28d40149866d68cc44f
REAZN <lewis@reazn.me>
2023-08-02 13:51:00 +0100

    ui styling

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,23 +1,25 @@
 import * as React from "react";
 import { Slot } from "@radix-ui/react-slot";
 import { cva, type VariantProps } from "class-variance-authority";
+import { Loader2Icon } from "lucide-react";
 
 import { cn } from "@/lib/utils";
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:opacity-50 disabled:pointer-events-none ring-offset-background",
+  "inline-flex items-center gap-2 justify-center rounded-md text-sm font-medium transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:opacity-50 disabled:pointer-events-none ring-offset-background",
   {
     variants: {
       variant: {
         default: "bg-primary text-primary-foreground hover:brightness-110",
         destructive:
-          "bg-red-600 text-destructive-foreground hover:brightness-110",
+          "bg-destructive text-destructive-foreground hover:brightness-105",
         outline:
           "border border-input hover:bg-accent hover:text-accent-foreground",
         secondary:
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "underline-offset-4 hover:underline text-primary",
+        loading: "bg-accent cursor-not-allowed",
       },
       size: {
         default: "h-9 py-2 px-3",
@@ -48,7 +50,16 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
         className={cn(buttonVariants({ variant, size, className }))}
         ref={ref}
         {...props}
-      />
+      >
+        {variant === "loading" ? (
+          <>
+            <Loader2Icon className="w-4 h-4 animate-spin" />
+            <span>{props.children}</span>
+          </>
+        ) : (
+          <>{props.children}</>
+        )}
+      </Comp>
     );
   }
 );
